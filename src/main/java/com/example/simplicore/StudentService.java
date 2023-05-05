59417ccbb21c30b8a776016cf7e37e69a8b94500
Suraj <suraj.kumar@arittek.com>
2023-05-05 15:32:13 +0500

    Data base connectivity

---
+++ src/main/java/com/example/simplicore/StudentService.java
@@ -0,0 +1,26 @@
+package com.example.simplicore;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepo studentRepo;
+
+    public void addStudents(StudentBean studentBean) {
+       Student student = Student.builder()
+               .name(studentBean.getName())
+               .email(studentBean.getEmail())
+               .mobileNo(studentBean.getMobileNo())
+               .build();
+       studentRepo.save(student);
+    }
+
+    public List<Student> getStudents() {
+        return studentRepo.findAll();
+    }
+}
