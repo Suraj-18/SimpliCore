6aaa3bdbd86594e85a19a9bd105fed23065751a0
Suraj <suraj.kumar@arittek.com>
2023-05-05 16:05:40 +0500

    Log4j Implimentation

--- src/main/java/com/example/simplicore/TestController.java
+++ src/main/java/com/example/simplicore/TestController.java
@@ -1,25 +1,30 @@
 package com.example.simplicore;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping
 public class TestController {
 
     @GetMapping(value = "/")
     public String welcome() {
+        log.info("Root Api Called");
         return "Hi welcome back";
     }
 
     @GetMapping(value = "/pipeline")
     public String pipeline() {
+        log.info("Pipeline Api Called");
         return "ci cd pipeline is working";
     }
 
     @GetMapping(value = "/pipeline/v2")
     public String pipelineV2() {
+        log.info("Pipeline Api 2 Called");
         return "ci cd pipelineV2 is working";
     }
 
