d5147b5eef55cc388dda81bb957f043e9946a04c
Suraj <suraj.kumar@arittek.com>
2023-05-08 13:10:37 +0500

    Student Get Api

--- src/main/java/com/example/simplicore/StudentController.java
+++ src/main/java/com/example/simplicore/StudentController.java
@@ -1,28 +1,28 @@
-//package com.example.simplicore;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping
-//@RequiredArgsConstructor
-//public class StudentController {
-//
-//    private final StudentService studentService;
-//
+package com.example.simplicore;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class StudentController {
+
+
 //    @PostMapping(value = "/student")
 //    public String saveStudent(@RequestBody StudentBean studentBean) {
 //        studentService.addStudents(studentBean);
 //        return "Successfully added";
 //    }
-//
-//    @GetMapping(value = "/student")
-//    public ResponseEntity<List<StudentBean>> getStudent() {
+
+    @GetMapping(value = "/student")
+    public ResponseEntity<StudentBean> getStudent() {
+        StudentBean studentBean = new StudentBean(1,"suraj","lohana.suraj66@gmail.com","03322824954");
 //       List<Student> studentList =  studentService.getStudents();
 //       List<StudentBean> studentBeans = studentList.stream().map(StudentBean ::new).toList();
-//       return ResponseEntity.ok(studentBeans);
-//    }
-//}
+       return ResponseEntity.ok(studentBean);
+    }
+}
